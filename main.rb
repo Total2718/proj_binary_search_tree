d563e389812a58a5d2054ad683f5e1f9e495834a
Total2718 <jlacenere@outlook.com>
2021-09-02 14:06:46 -0400

    After thorough trial and error testing, the delete method has been complete. It deletes moves partiular values upwards along the tree in such a way that it still makes it a valid bst

--- main.rb
+++ main.rb
@@ -47,7 +47,7 @@ class Tree
 
 
     end
-    def insert(value, node=@root)
+    def insert(node=@root, value)
 
         if node.data == value
             puts "This data point already exists."
@@ -67,43 +67,57 @@ class Tree
         end
     end
 
-    def delete(value)
-        if @root.data == value
-            node = @root
-        else
-            node = locate(value)
+    def delete(value, node=@root)
+        puts value
+        puts "break"
+        puts node.data
+        if node.data != value
+            puts "here"
+            node = locate(value, node)
+            puts node.data
         end
 
         if node == nil 
-            puts "The value doesn't exist."
-        elsif node == @root
+            return nil
+        elsif node.data == value
             if node.left != nil && node.right != nil
-               
+                temp = in_order_successor(node)
+                node.data = temp.data
+                puts "Hello"
+                delete(temp.data, node.right)
             elsif node.left != nil
-
-            elsif node.right != nil
-
-            else
-                @root = nil
+                temp = node.left
+                node.data = temp.data
+                puts "Howdy"
+                delete(temp.data, node.left)
+            elsif node.right != nil 
+                temp = node.right
+                node.data = temp.data
+                puts "Hi"
+                puts node.data
+                delete(temp.data, node.right)
             end
-        elsif node.right == value
+        elsif node.right.data == value
             if node.right.right != nil && node.right.left != nil
-
+                temp = in_order_successor(node.right.right)
+                node.right.data = temp.data
+                delete(temp.data, node.right.right)
             elsif node.right.right != nil
-
+                node.right = node.right.right
             elsif node.right.left != nil
-
+                node.right = node.right.left
             else
                 node.right = nil
             end
-        elsif node.left == value
-            if node.left.right != nil && node.right.left != nil
-
-            elsif node.left.right != nil
-                node.left = node.left.right
-
+        elsif node.left.data == value
+            if node.left.right != nil && node.left.left != nil
+                temp = in_order_successor(node.left.right)
+                node.left.data = temp.data
+                delete(temp.data, node.left.right)
             elsif node.left.left != nil
                 node.left = node.left.left
+            elsif node.left.right != nil
+                node.left = node.left.right
             else
                 node.left = nil
             end
@@ -112,11 +126,14 @@ class Tree
         end
     end
     def locate(value, node=@root)
+       
         if node.data < value
             if node.right != nil
                 if node.right.data == value
                     return node
+                    puts "option 1"
                 else
+                    puts "option 2"
                     locate(value, node.right)
                 end
             else
@@ -125,8 +142,10 @@ class Tree
         elsif node.data > value
             if node.left != nil
                 if node.left.data == value
+                    puts "option 3"
                     return node
                 else
+                    puts "option 4"
                     locate(value, node.left)
                 end
             else
@@ -135,22 +154,33 @@ class Tree
         end
     end
 
-    def min_value()
-        
-    
+    def in_order_successor(current)
+        current = current.right
+        while current.left != nil
+            current = current.left
+        end
+
+        return current
     end
 
     def pretty_print(node = @root, prefix = '', is_left = true)
         pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
         puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
         pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
-      end
+    end
 end
 
-
+def space
+    5.times{puts "\n"}
+end
 test_array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
 my_bst = Tree.new(test_array)
 my_bst.pretty_print
-puts test_array.sort.uniq
+space
+#puts test_array.sort.uniq
 my_bst.insert(53)
+
+my_bst.pretty_print
+space
+my_bst.delete(1)
 my_bst.pretty_print
